25f313d9b63ac600a436f24564c9bbe2f7645015
richardtam.ny <richardtam.ny@gmail.com>
2018-12-02 18:00:04 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {artistName: "songTitle"}
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.Slowdrive = "My Bloody Valentine"
+  playlist.MyBloodyValentine = "Phil Ochs"
+  playlist.PhilOchs = "Slowdive"
+  return playlist
+}
